e682f4d1ca51c0f8ad07681ca0b33280a7b90553
Pavel Milanes (CO7WT) <dev0032@simelo.tech>
2020-04-22 14:57:46 -0400

    Export data as .vcf files, improve docstrings and documentation

--- mdexportab.py
+++ mdexportab.py
@@ -12,35 +12,23 @@
 #########################################################################################
 
 import os
-import xml.etree.ElementTree as ET
-
-DEBUG = False
+from xml.etree import ElementTree as ET
+from quopri import encodestring as qpencode
 
 def detect_folders_inside(fpath):
     """Detect folders inside a specific path, retuned as a list"""
 
-    if DEBUG:
-        print(":: Parsing folder {}".format(fpath))
-    
     folders = []
     for d in os.listdir(fpath):
-        if DEBUG:
-            print("::: {}".format(d))
-        
         if os.path.isdir(os.path.join(fpath, d)):
             folders.append(d)
-        else:
-            if DEBUG:
-                print("::: Discard file: {}".format(d))
 
     return folders
 
 def get_mrk_files(fpath):
-    """Get the addressbook files and parse it on an array indexed by the username
-    return that array"""
+    """Get the addressbook files for a given path"""
 
     files = []
-
     # r=root, d=directories, f= files
     for r, d, f in os.walk(fpath):
         for file in f:
@@ -53,18 +41,39 @@ def parse_mrk_file(userab, ab):
     """Parse the ab file, ignoring already parsed entries"""
 
     for c in ab.iter('contact'):
+        contact = []
         try:
             name = c.find('fullName').text
             email = c.find('email').text
-
-            # final string
-            entry = name + ";" + email + ";"
-            if not entry in userab:
-                userab.append(entry)
         except:
             pass
+            continue
+
+        # create the contact list
+        if email != "":
+            contact.append(name)
+            contact.append(email)
+
+        # append if not already
+        if not contact in userab:
+            userab.append(contact)
 
     return userab
+
+def vcfWriter(contact):
+    """Adapted from the file format produced by mozilla thunderbird and some examples online"""
+
+    name, email = contact
+    vcfLines = []
+    vcfLines.append('begin:vcard')
+    vcfLines.append('version:2.1')
+    if name != email:
+        ename = qpencode(name.encode()).decode()
+        vcfLines.append('fn;quoted-printable:{}'.format(ename))
+    vcfLines.append('email;internet:{}'.format(email))
+    vcfLines.append('end:vcard')
+    vcfString = '\n'.join(vcfLines) + '\n\n'
+    return vcfString
 
 if __name__ == "__main__":
     # work on local path
@@ -112,7 +121,8 @@ if __name__ == "__main__":
             # notice
             print("User: {}, rescued {} contacts".format(u, len(userab)))
             # write the file for that user
-            uf = os.path.join(opath, u + ".csv")
+            uf = os.path.join(opath, u + ".vcf")
             print(uf)
             with open(uf, mode='wt', encoding='utf-8') as myfile:
-                myfile.write('\n'.join(userab))
+                for c in userab:
+                    myfile.write(vcfWriter(c))
